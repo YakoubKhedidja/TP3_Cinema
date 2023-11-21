569e755b7ed16dab89a6feeaf5162921ba335707
YakoubKhedidja <azad.ampere@gmail.com>
2023-11-21 01:49:56 -0500

    mise à jour mineur
    J'ai oublié de mettre en commentaire la configuration pour WebDev.

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
-define('PATH_DIR', 'https://e2295558.webdev.cmaisonneuve.qc.ca/TP3_CinemaWebDev/'); 
+// ======================= configuration pour le webdev =====================================
+//
+// define('PATH_DIR', 'https://e2295558.webdev.cmaisonneuve.qc.ca/TP3_CinemaWebDev/'); 
+//
+// require_once('app/controleurs/Frontend.Class.php');
+// require_once __DIR__.'app/vues/vendor/autoload.php';
 
-require_once('app/controleurs/Frontend.Class.php');
-require_once __DIR__.'app/vues/vendor/autoload.php';
+// $url = isset($_GET["url"]) ? explode ('/', ltrim($_GET["url"], '/')) : '/';
+//
+// ===========================================================================================
 
 require 'app/includes/config.php';
 require 'app/includes/chargementClasses.inc.php';
 
-$url = isset($_GET["url"]) ? explode ('/', ltrim($_GET["url"], '/')) : '/';
 
 
 session_start();
